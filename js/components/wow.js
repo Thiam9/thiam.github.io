f7774d1271158064cc1c90da66b4e8a6dcfd2446
Ibrahima Thiam <66336737+Thiam9@users.noreply.github.com>
2020-11-23 16:49:37 +0100

    Add files via upload

---
+++ js/components/wow.js
@@ -0,0 +1,25 @@
+var Wow = function() {
+    "use strict";
+
+
+    var handleWow = function() {
+        var wow = new WOW({
+            boxClass: 'wow',
+            offset: 0,
+            mobile: false,
+            tablet: false
+        });
+        wow.init();
+    }
+
+    return {
+        init: function() {
+            handleWow();
+            r
+        }
+    }
+}();
+
+$(document).ready(function() {
+    Wow.init();
+});
